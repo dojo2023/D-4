35a8405ae0e47629c03847d2581f5b7e92fff917
dojo2023-shinozaki <130331048+dojo2023-shinozaki@users.noreply.github.com>
2023-06-21 17:50:03 +0900

    Update files

--- src/WebContent/WEB-INF/jsp/GoalRegist.jsp
+++ src/WebContent/WEB-INF/jsp/GoalRegist.jsp
@@ -1,9 +1,14 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <!DOCTYPE html>
 
 <head>
-	<meta charset="UTF-8">
-	<title>目標追加</title>
+<meta charset="UTF-8">
+<title>目標追加</title>
+
+<link rel="stylesheet" href="/simpleBC/SimpleBC_css/common.css">
+<link rel="stylesheet" href="/simpleBC/SimpleBC_css/goal_regist.css">
 </head>
 
 <body>
@@ -25,18 +30,32 @@
 		<!-- メイン -->
 		<main class="main">
 			<form id="dataForm" action="GoalRegistServlet" method="POST">
-			<!-- 現在の年月を表示 -->
-
-			<!-- 年月を遷移するためのボタン -->
+				<!-- 現在の年月を表示 -->
+				<div class="monthMove">
+					<div class="monthcontent">
+						<!-- //の中に動かしたいプロジェクト名を入れてください -->
+						<a href='/simpleBC/GoalLastMonthServlet' class="prev"></a>
+					</div>
+					<div class=monthcontent>
+						<h3>
+							<c:out value="${displayYear}" />
+							年
+							<c:out value="${displayMonth}" />
+							月
+						</h3>
+					</div>
+					<div class="monthcontent">
+						<a href='/simpleBC/GoalNextMonthServlet' class="next"></a>
+					</div>
+				</div>
 
 				<h1>目標追加</h1>
 				<!-- 長期目標 -->
-				<label>長期目標</label>
-				<input id="lgInput" type="text" name="lg"><br>
+				<label>長期目標</label> <input id="lgInput" type="text" name="lg"><br>
 				<!-- 短期目標１ -->
-				<label>短期目標１</label>
-				<input id="sgInput1" type="text" name="sg1"><input type="date" id="sgStart" name="day_s_1"  placeholder="開始">
-  															<input type="date" id="sgEnd" name="day_e_1"  placeholder="終了"><br>
+				<label>短期目標１</label> <input id="sgInput1" type="text" name="sg1"><input
+					type="date" id="sgStart" name="day_s_1" placeholder="開始"> <input
+					type="date" id="sgEnd" name="day_e_1" placeholder="終了"><br>
 				<!-- 短期目標１のToDoグループ -->
 				<input id="tdInput1_1" type="text" name="td1_1" placeholder="todo"><br>
 				<input id="tdInput1_2" type="text" name="td1_2" placeholder="todo"><br>
@@ -44,8 +63,9 @@
 				<input id="tdInput1_4" type="text" name="td1_4" placeholder="todo"><br>
 				<input id="tdInput1_5" type="text" name="td1_5" placeholder="todo"><br>
 				<!-- 短期目標２ -->
-				<label>短期目標２</label>
-				<input id="sgInput2" type="text" name="sg2"><br>
+				<label>短期目標２</label> <input id="sgInput2" type="text" name="sg2"><input
+					type="date" id="sgStart" name="day_s_2" placeholder="開始"> <input
+					type="date" id="sgEnd" name="day_e_2" placeholder="終了"><br>
 				<!-- 短期目標２のToDoグループ -->
 				<input id="tdInput2_1" type="text" name="td2_1" placeholder="todo"><br>
 				<input id="tdInput2_2" type="text" name="td2_2" placeholder="todo"><br>
@@ -53,8 +73,9 @@
 				<input id="tdInput2_4" type="text" name="td2_4" placeholder="todo"><br>
 				<input id="tdInput2_5" type="text" name="td2_5" placeholder="todo"><br>
 				<!-- 短期目標３ -->
-				<label>短期目標３</label>
-				<input id="sgInput3" type="text" name="sg3"><br>
+				<label>短期目標３</label> <input id="sgInput3" type="text" name="sg3"><input
+					type="date" id="sgStart" name="day_s_3" placeholder="開始"> <input
+					type="date" id="sgEnd" name="day_e_3" placeholder="終了"><br>
 				<!-- 短期目標３のToDoグループ -->
 				<input id="tdInput3_1" type="text" name="td3_1" placeholder="todo"><br>
 				<input id="tdInput3_2" type="text" name="td3_2" placeholder="todo"><br>
@@ -62,8 +83,9 @@
 				<input id="tdInput3_4" type="text" name="td3_4" placeholder="todo"><br>
 				<input id="tdInput3_5" type="text" name="td3_5" placeholder="todo"><br>
 				<!-- 短期目標４ -->
-				<label>短期目標４</label>
-				<input id="sgInput4" type="text" name="sg4"><br>
+				<label>短期目標４</label> <input id="sgInput4" type="text" name="sg4"><input
+					type="date" id="sgStart" name="day_s_4" placeholder="開始"> <input
+					type="date" id="sgEnd" name="day_e_4" placeholder="終了"><br>
 				<!-- 短期目標４のToDoグループ -->
 				<input id="tdInput4_1" type="text" name="td4_1" placeholder="todo"><br>
 				<input id="tdInput4_2" type="text" name="td4_2" placeholder="todo"><br>
@@ -71,8 +93,9 @@
 				<input id="tdInput4_4" type="text" name="td4_4" placeholder="todo"><br>
 				<input id="tdInput4_5" type="text" name="td4_5" placeholder="todo"><br>
 				<!-- 短期目標５ -->
-				<label>短期目標５</label>
-				<input id="sgInput5" type="text" name="sg5"><br>
+				<label>短期目標５</label> <input id="sgInput5" type="text" name="sg5"><input
+					type="date" id="sgStart" name="day_s_5" placeholder="開始"> <input
+					type="date" id="sgEnd" name="day_e_5" placeholder="終了"><br>
 				<!-- 短期目標５のToDoグループ -->
 				<input id="tdInput5_1" type="text" name="td5_1" placeholder="todo"><br>
 				<input id="tdInput5_2" type="text" name="td5_2" placeholder="todo"><br>
@@ -84,8 +107,6 @@
 			</form>
 		</main>
 		<!-- フッター -->
-		<footer class="footer">
-
-		</footer>
+		<footer class="footer"> </footer>
 	</div>
 </body>
--- src/src/servlet/GoalRegistServlet.java
+++ src/src/servlet/GoalRegistServlet.java
@@ -8,10 +8,22 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dao.LgDAO;
+import dao.SgDAO;
+import model.Lg;
+import model.Sg;
+
+import dao.TodotbDAO;
+import model.Todo;
+
 @WebServlet("/GoalRegistServlet")
 public class GoalRegistServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+
+		//スコープからNUMBERを取得
+		int number = Integer.parseInt(request.getParameter("NUMBER"));
+
 		// フォームデータの受け取り
 		// 長期目標
 		String lg = request.getParameter("lg");
@@ -29,6 +41,8 @@ public class GoalRegistServlet extends HttpServlet {
 
 		//短期目標２
 		String sg2 = request.getParameter("sg2");
+		String day_s_2 = request.getParameter("day_s_2");
+		String day_e_2 = request.getParameter("day_e_2");
 		//短期目標２のToDo
 		String td2_1 = request.getParameter("td2_1");
 		String td2_2 = request.getParameter("td2_2");
@@ -38,6 +52,8 @@ public class GoalRegistServlet extends HttpServlet {
 
 		//短期目標３
 		String sg3 = request.getParameter("sg3");
+		String day_s_3 = request.getParameter("day_s_3");
+		String day_e_3 = request.getParameter("day_e_3");
 		//短期目標３のToDo
 		String td3_1 = request.getParameter("td3_1");
 		String td3_2 = request.getParameter("td3_2");
@@ -47,6 +63,8 @@ public class GoalRegistServlet extends HttpServlet {
 
 		//短期目標４
 		String sg4 = request.getParameter("sg4");
+		String day_s_4 = request.getParameter("day_s_4");
+		String day_e_4 = request.getParameter("day_e_4");
 		//短期目標４のToDo
 		String td4_1 = request.getParameter("td4_1");
 		String td4_2 = request.getParameter("td4_2");
@@ -56,6 +74,8 @@ public class GoalRegistServlet extends HttpServlet {
 
 		//短期目標５
 		String sg5 = request.getParameter("sg5");
+		String day_s_5 = request.getParameter("day_s_5");
+		String day_e_5 = request.getParameter("day_e_5");
 		//短期目標５のToDo
 		String td5_1 = request.getParameter("td5_1");
 		String td5_2 = request.getParameter("td5_2");
@@ -63,21 +83,19 @@ public class GoalRegistServlet extends HttpServlet {
 		String td5_4 = request.getParameter("td5_4");
 		String td5_5 = request.getParameter("td5_5");
 
-		//スコープからNUMBERを取得
-
-	    //int number = Integer.parseInt(request.getParameter("NUMBER"));
 
 		//nullチェック（lg sg td）
 		// 受け取ったデータの表示
+		//データの処理
 
 		//長期目標
 		if (lg != null && lg != "") {
 			System.out.println("長期目標: " + lg);
 
-		//number, year, month, lgをLGOALに登録
+			//number, year, month, lgをLGOALに登録
 
-			//LgDAO lgDao = new LgDAO();
-		    //Lg lgs = new Lg(number, year, month, lg);
+			LgDAO lgDao = new LgDAO();
+			Lg lgs = new Lg(number, year, month, lg);
 
 		}
 		//短期目標１
@@ -85,103 +103,199 @@ public class GoalRegistServlet extends HttpServlet {
 			System.out.println("短期目標１: " + sg1);
 			System.out.println(day_s_1);
 			System.out.println(day_e_1);
+
+			SgDAO sgDao = new SgDAO();
+			Sg sgs = new Sg(number, month, day_s_1, day_e_1, sg1);
 		}
 		//短期目標１のToDo
 		if (td1_1 != null && td1_1 != "") {
 			System.out.println("短期目標１のToDo1: " + td1_1);
+
+			TodotbDAO todotbDao = new TodotbDAO();
+			Todo todo = new Todo(number, month, day_s_1, day_e_1, td1_1);
 		}
 		if (td1_2 != null && td1_2 != "") {
 			System.out.println("短期目標１のToDo2: " + td1_2);
+
+			TodotbDAO todotbDao = new TodotbDAO();
+			Todo todo = new Todo(number, month, day_s_1, day_e_1, td1_2);
 		}
 		if (td1_3 != null && td1_2 != "") {
 			System.out.println("短期目標１のToDo3: " + td1_3);
+
+			TodotbDAO todotbDao = new TodotbDAO();
+			Todo todo = new Todo(number, month, day_s_1, day_e_1, td1_3);
 		}
 		if (td1_4 != null && td1_4 != "") {
 			System.out.println("短期目標１のToDo4: " + td1_4);
+
+			TodotbDAO todotbDao = new TodotbDAO();
+			Todo todo = new Todo(number, month, day_s_1, day_e_1, td1_4);
 		}
 		if (td1_5 != null && td1_5 != "") {
 			System.out.println("短期目標１のToDo5: " + td1_5);
 
+			TodotbDAO todotbDao = new TodotbDAO();
+			Todo todo = new Todo(number, month, day_s_1, day_e_1, td1_5);
+		}
+
 			//短期目標２
 			if (sg2 != null && sg2 != "") {
 				System.out.println("短期目標２: " + sg2);
+
+				SgDAO sgDao = new SgDAO();
+				Sg sgs = new Sg(number, month, day_s_2, day_e_2, sg2);
+
 			}
 			//短期目標２のToDo
 			if (td2_1 != null && td2_1 != "") {
 				System.out.println("短期目標２のToDo1: " + td2_1);
+
+				TodotbDAO todotbDao = new TodotbDAO();
+				Todo todo = new Todo(number, month, day_s_2, day_e_2, td2_1);
 			}
 			if (td2_2 != null && td2_2 != "") {
 				System.out.println("短期目標２のToDo2: " + td2_2);
+
+				TodotbDAO todotbDao = new TodotbDAO();
+				Todo todo = new Todo(number, month, day_s_2, day_e_2, td2_2);
 			}
 			if (td2_3 != null && td2_3 != "") {
 				System.out.println("短期目標２のToDo3: " + td2_3);
+
+				TodotbDAO todotbDao = new TodotbDAO();
+				Todo todo = new Todo(number, month, day_s_2, day_e_2, td2_3);
 			}
 			if (td2_4 != null && td2_4 != "") {
 				System.out.println("短期目標２のToDo4: " + td2_4);
+
+				TodotbDAO todotbDao = new TodotbDAO();
+				Todo todo = new Todo(number, month, day_s_2, day_e_2, td2_4);
 			}
 			if (td2_5 != null && td2_5 != "") {
 				System.out.println("短期目標２のToDo5: " + td2_5);
 
+				TodotbDAO todotbDao = new TodotbDAO();
+				Todo todo = new Todo(number, month, day_s_2, day_e_2, td2_5);
+			}
 				//短期目標３
 				if (sg3 != null && sg3 != "") {
 					System.out.println("短期目標３: " + sg3);
+
+					SgDAO sgDao = new SgDAO();
+					Sg sgs = new Sg(number, month, day_s_3, day_e_3, sg3);
+
 				}
 				//短期目標３のToDo
 				if (td3_1 != null && td3_1 != "") {
 					System.out.println("短期目標３のToDo1: " + td3_1);
+
+					TodotbDAO todotbDao = new TodotbDAO();
+					Todo todo = new Todo(number, month, day_s_3, day_e_3, td3_1);
 				}
 				if (td3_2 != null && td3_2 != "") {
 					System.out.println("短期目標３のToDo2: " + td3_2);
+
+					TodotbDAO todotbDao = new TodotbDAO();
+					Todo todo = new Todo(number, month, day_s_3, day_e_3, td3_2);
 				}
 				if (td3_3 != null && td3_3 != "") {
 					System.out.println("短期目標３のToDo3: " + td3_3);
+
+					TodotbDAO todotbDao = new TodotbDAO();
+					Todo todo = new Todo(number, month, day_s_3, day_e_3, td3_3);
 				}
 				if (td3_4 != null && td3_4 != "") {
 					System.out.println("短期目標３のToDo4: " + td3_4);
+
+					TodotbDAO todotbDao = new TodotbDAO();
+					Todo todo = new Todo(number, month, day_s_3, day_e_3, td3_4);
 				}
 				if (td3_5 != null && td3_5 != "") {
 					System.out.println("短期目標３のToDo5: " + td3_5);
 
+					TodotbDAO todotbDao = new TodotbDAO();
+					Todo todo = new Todo(number, month, day_s_3, day_e_3, td3_5);
+				}
+
 					//短期目標４
 					if (sg4 != null && sg4 != "") {
 						System.out.println("短期目標４: " + sg4);
+
+						SgDAO sgDao = new SgDAO();
+						Sg sgs = new Sg(number, month, day_s_4, day_e_4, sg4);
+
 					}
 					//短期目標４のToDo
 					if (td4_1 != null && td4_1 != "") {
 						System.out.println("短期目標４のToDo1: " + td4_1);
+
+						TodotbDAO todotbDao = new TodotbDAO();
+						Todo todo = new Todo(number, month, day_s_4, day_e_4, td4_1);
 					}
 					if (td4_2 != null && td4_2 != "") {
 						System.out.println("短期目標４のToDo2: " + td4_2);
+
+						TodotbDAO todotbDao = new TodotbDAO();
+						Todo todo = new Todo(number, month, day_s_4, day_e_4, td4_2);
 					}
 					if (td4_3 != null && td4_3 != "") {
 						System.out.println("短期目標４のToDo3: " + td4_3);
+
+						TodotbDAO todotbDao = new TodotbDAO();
+						Todo todo = new Todo(number, month, day_s_4, day_e_4, td4_3);
 					}
 					if (td4_4 != null && td4_4 != "") {
 						System.out.println("短期目標４のToDo4: " + td4_4);
+
+						TodotbDAO todotbDao = new TodotbDAO();
+						Todo todo = new Todo(number, month, day_s_4, day_e_4, td4_4);
 					}
 					if (td4_5 != null && td4_5 != "") {
 						System.out.println("短期目標４のToDo5: " + td4_5);
 
+						TodotbDAO todotbDao = new TodotbDAO();
+						Todo todo = new Todo(number, month, day_s_4, day_e_4, td4_5);
+					}
+
 						//短期目標５
 						if (sg5 != null && sg5 != "") {
 							System.out.println("短期目標５: " + sg5);
+
+							SgDAO sgDao = new SgDAO();
+							Sg sgs = new Sg(number, month, day_s_5, day_e_5, sg5);
+
 						}
 						//短期目標５のToDo
 						if (td5_1 != null && td5_1 != "") {
 							System.out.println("短期目標５のToDo1: " + td5_1);
+
+							TodotbDAO todotbDao = new TodotbDAO();
+							Todo todo = new Todo(number, month, day_s_5, day_e_5, td5_1);
 						}
 						if (td5_2 != null && td5_2 != "") {
 							System.out.println("短期目標５のToDo2: " + td5_2);
+
+							TodotbDAO todotbDao = new TodotbDAO();
+							Todo todo = new Todo(number, month, day_s_5, day_e_5, td5_2);
 						}
 						if (td5_3 != null && td5_3 != "") {
 							System.out.println("短期目標５のToDo3: " + td5_3);
+
+							TodotbDAO todotbDao = new TodotbDAO();
+							Todo todo = new Todo(number, month, day_s_5, day_e_5, td5_3);
 						}
 						if (td5_4 != null && td5_4 != "") {
 							System.out.println("短期目標５のToDo4: " + td5_4);
+
+							TodotbDAO todotbDao = new TodotbDAO();
+							Todo todo = new Todo(number, month, day_s_5, day_e_5, td5_4);
 						}
 						if (td5_5 != null && td5_5 != "") {
 							System.out.println("短期目標５のToDo5: " + td5_5);
 
+							TodotbDAO todotbDao = new TodotbDAO();
+							Todo todo = new Todo(number, month, day_s_5, day_e_5, td5_5);
 						}
 						// 他の処理を追加することも可能
 
