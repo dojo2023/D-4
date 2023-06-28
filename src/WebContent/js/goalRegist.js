e0a4a69506500103f541a7a4dde3156fdb9b2dae
dojo2023-shinozaki <130331048+dojo2023-shinozaki@users.noreply.github.com>
2023-06-28 17:33:08 +0900

    Update files

--- src/WebContent/js/goalRegist.js
+++ src/WebContent/js/goalRegist.js
@@ -5,19 +5,19 @@
 
   document.getElementById("dataForm").addEventListener("submit", function(event) {
     var lgInput = document.getElementsByName("lg");
-    var sgInput = document.getElementsByName("sg");
+    var sgInput1 = document.getElementsByName("sg1");
 
-    if (lgInput.equals("")) {
+    if (lgInput === "") {
       event.preventDefault(); // フォームの送信を中止
 
       alert("長期目標を記入してください"); // ポップアップメッセージを表示
     }
 
-    if (sgInput1.equals("") && !tdInput1_1.equals("") ||
-		sgInput1.equals("") && !tdInput1_2.equals("") ||
-		sgInput1.equals("") && !tdInput1_3.equals("") ||
-		sgInput1.equals("") && !tdInput1_4.equals("") ||
-		sgInput1.equals("") && !tdInput1_5.equals("") ) {
+    if (sgInput1 === "" && !tdInput1_1 === "" ||
+		sgInput1 === "" && !tdInput1_2 === "" ||
+		sgInput1 === "" && !tdInput1_3 === "" ||
+		sgInput1 === "" && !tdInput1_4 === "" ||
+		sgInput1 === "" && !tdInput1_5 === "" ) {
       event.preventDefault(); // フォームの送信を中止
 
       alert("短期目標１を記入してください"); // ポップアップメッセージを表示
